d66da68977bb497bf19acef3c78bbd5aa93e2cbf
ertugrul gurgen <88859001+ErtugrulGurgen@users.noreply.github.com>
2022-04-25 23:21:34 +0200

    Add files via upload

---
+++ src/auth/firebase.js
@@ -0,0 +1,76 @@
+import { async } from "@firebase/util";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export const createUser = async (email, password, navigate, displayName) => {
+  try {
+    let credentials = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+      );
+    await updateProfile(auth.currentUser, 
+    {
+      displayName: displayName,
+    });
+    navigate("/");
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export const signInUser = async (email, password, navigate) => {
+  try {
+    let signIn = await signInWithEmailAndPassword(auth, email, password);
+    console.log(signIn);
+    navigate("/");
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export const signOutUser = async () => {
+    await signOut(auth)}; 
+
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser)=> {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+    } else {
+      setCurrentUser(false);
+    }
+  });
+}
+
+export const signUpProvider = (navigate) => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+  .then((result) => {
+    console.log(result);
+    navigate("/");
+  }).catch((error) => {
+   alert(error.message);
+  });
+}
