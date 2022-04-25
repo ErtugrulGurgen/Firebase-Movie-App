c848ab601784d0f7a398bad49250e5df9decf49d
ertugrul gurgen <88859001+ErtugrulGurgen@users.noreply.github.com>
2022-04-25 22:33:39 +0200

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { signOutUser } from "../auth/firebase";
+import { AuthContext } from "../context/AuthContext";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const {currentUser} = useContext(AuthContext);
+
+  // const currentUser = {displayName : "Ertugrul"};
+  // const currentUser = false;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to={"/"} className="navbar-brand text-white">
+            <h4>React Movie App</h4>
+          </Link>
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <h5 className="mb-0 text-capitalize">
+                {currentUser?.displayName}
+              </h5>
+            ) : (
+              <button
+                className="ms-2 btn btn-outline-light"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+            )}
+            {currentUser ? (
+              <button
+                className="ms-2 btn btn-outline-light"
+                onClick={() => signOutUser()}
+              >
+                Logout
+              </button>
+            ) : (
+              <button
+                className="ms-2 btn btn-outline-light"
+                onClick={() => navigate("/register")}
+              >
+                Register
+              </button>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
